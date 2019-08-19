e771054533f4119c04777044292c7f15a838ff0c
dozensofdars <yuuya4000s@gmail.com>
2019-08-19 21:06:10 +0900

    requestURL.searchParams を param 変数に入れて簡潔化

--- server.js
+++ server.js
@@ -4,21 +4,22 @@ const fetch = require('node-fetch');
 const getData = async(url) => {
     const localURL = new URL('http://yumuta.github.io/vocaminer/'+url);
     const requestURL = new URL('http://api.search.nicovideo.jp/api/v2/video/contents/search');
-    requestURL.searchParams.set('q', localURL.searchParams.get('q') || 'ミクオリジナル曲');
-    requestURL.searchParams.set('targets', 'tagsExact');
-    requestURL.searchParams.set('filters[viewCounter][gte]', localURL.searchParams.get('viewgte') || 100);
-    requestURL.searchParams.set('filters[viewCounter][lte]', localURL.searchParams.get('viewlte') || 1000);
-    requestURL.searchParams.set('_limit', 1);
+    const param = requestURL.searchParams;
+    param.set('q', localURL.searchParams.get('q') || 'ミクオリジナル曲');
+    param.set('targets', 'tagsExact');
+    param.set('filters[viewCounter][gte]', localURL.searchParams.get('viewgte') || 100);
+    param.set('filters[viewCounter][lte]', localURL.searchParams.get('viewlte') || 1000);
+    param.set('_limit', 1);
     // TODO: ソート方法をランダマイズ
-    requestURL.searchParams.set('_sort', '-lastCommentTime');
+    param.set('_sort', '-lastCommentTime');
     // TODO: contextはvocaminerでよさそう
-    requestURL.searchParams.set('_context', 'apiguide');
+    param.set('_context', 'apiguide');
     return new Promise(async(resolve, reject)=>{
         try {
             const res = await fetch(requestURL);
             const count = await res.headers.get('x-total-count');
-            requestURL.searchParams.set('fields', 'contentId,title');
-            requestURL.searchParams.set('_offset', getOffset(count));
+            param.set('fields', 'contentId,title');
+            param.set('_offset', getOffset(count));
             const res2 = await fetch(requestURL);
             const json = await res2.json();
             if (json.meta.status !== 200) return reject('response is not 200');
