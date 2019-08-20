d4fb48beabb32c622179c6c71274d0fc5763a7b2
dozensofdars <yuuya4000s@gmail.com>
2019-08-20 20:43:31 +0900

    ソート方法をランダムに決定
    ・昇順or降順（order）
    ・ソート方法（sortMethod）

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ const getData = async(url) => {
     param.set('filters[viewCounter][gte]', localURL.searchParams.get('viewgte') || 100);
     param.set('filters[viewCounter][lte]', localURL.searchParams.get('viewlte') || 1000);
     param.set('_limit', 1);
-    // TODO: ソート方法をランダマイズ
-    param.set('_sort', '-lastCommentTime');
+    // ソート方法をランダマイズ
+    param.set('_sort', randomizedSortMethod());
     // TODO: contextはvocaminerでよさそう
     param.set('_context', 'apiguide');
     return new Promise(async(resolve, reject)=>{
@@ -33,8 +33,20 @@ const getData = async(url) => {
 
 const getOffset = (count) => {
     maxOffset = Math.min(1600, count);
-    return Math.floor(Math.random() * maxOffset);
+    return randInt(maxOffset);
 };
+
+const randomizedSortMethod = () => {
+    const order = ['+', '-'];
+    const sortMethod = [
+        'userId', 'viewCounter', 'mylistCounter', 'lengthSeconds', 'startTime',
+        'threadId', 'commentCounter', 'lastCommentTime', 'channelId'
+    ];
+    return order[randInt(order.length)] + sortMethod[randInt(sortMethod.length)];
+}
+
+// 0 以上 num 未満の整数をランダムに返す
+const randInt = num => Math.floor(Math.random() * num);
 
 const server = http.createServer((req, res) => {
     if(req.method !== 'GET') return;
